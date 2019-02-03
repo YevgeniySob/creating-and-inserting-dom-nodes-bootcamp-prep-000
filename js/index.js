2074fab04e95572850ea81beae1a6ea545b94970
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-03 07:41:10 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+var element = document.createElement('div');
+
+
 element.style.textAlign = 'center';
